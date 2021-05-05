c89b70cb4f3fc5897f626c16417aedc61124610a
Ali Kabalan <ali.kabalan.1994@gmail.com>
2021-05-05 20:31:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ function map(arr, funct) {
 // function reduce(src, cb, starting){
 //   let r = (!!starting) ? starting : src[0]
 //   let i = (!!starting) ? 0 : 1
-// 
+//
 //   for (; i < src.length; i++) {
 //     r = cb(src[i], r)
 //   }
-// 
+//
 //   return r;
 // }
