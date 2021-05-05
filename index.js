28d3ec98680e7f69a0e744960847ee5796859bb5
Ali Kabalan <ali.kabalan.1994@gmail.com>
2021-05-05 19:53:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your functions here
+// // Add your functions here
+//
+// function mapFn (array, funct) {
+//     let arr = [];
+//     for (let i = 0; i < array.length; i++){
+//         arr.push(funct(array[i]));
+//     }
+//     return arr;
+// }
+
+
+// function reduceFn (array, funct, start) {
+//     let tmp;
+//     if(start) {
+//         tmp = start;
+//         for (let i = 0; i < array.length; i++) {
+//             tmp = funcy(array[i], tmp);
+//         }
+//     }
+//     else {
+//         tmp = array[0];
+//         for (let i = 1; i < array.length; i++) {
+//             tmp = funcy(array[i], tmp);
+//         }
+//     }
+//     return tmp;
+// }
